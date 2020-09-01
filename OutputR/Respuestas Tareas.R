d0f336c759bce34b20f40cc942292db96edf478a
Alberto Rodríguez R <albrodriguezr@unal.edu.co>
2020-09-01 15:58:40 -0500

    Carga modulo matrices

---
+++ OutputR/Respuestas Tareas.R
@@ -0,0 +1,130 @@
+# TAREA 1 ----
+
+# P1. ----
+
+# Operación aritmética compuesta
+(6/2*(2+1 + 2^3) + 24/(2*3 + 0^3) + 3)/8
+
+
+# Paso 1
+(6/2*(3 + 8) + 24/(6 + 0) + 3)/8
+# Paso 2
+(6/2*11 + 24/6 + 3)/8
+# Paso 3
+(3*11 + 4 + 3)/8
+# Paso 4
+(33 + 4 + 3)/8
+# Paso 5
+40/8
+
+# P2. ----
+
+`longitud circunferencia vs radio` <- pi
+`longitud circunferencia vs radio`
+
+# P3. ----
+
+Var%_1 <- TRUE
+# Var_1 <- TRUE
+
+# P4. ----
+
+seq(from = 5, to = 100, by = 5)
+seq(from = 100, to = 5, by = -5)
+
+# P5. ----
+
+rep(c('a', 'b', 'c', 'd'), times = c(1:4))
+
+# P6. ----
+
+var_1 <- c(1:10)
+var_2 <- c(1, 0)
+Rta <- var_1 / var_2
+?rep
+
+<- 
+
+# P7. ----
+
+x <- c(10, 15, 20 , '30')
+y <- c(2, 5, '2', 10)
+x*y
+
+# Vs
+
+x <- c(10, 15, 20 , 30)
+y <- c(2, 5, 2, 10)
+x*y
+
+# P8. ----
+
+m <- c(1:100)
+m <= 10 
+
+# P9. ----
+
+Números <- c('uno', 'tres', 'uno ', 'cuatro', ' uno', 'cinco', 'uno')
+Resultado <- Números == 'uno'
+Resultado
+
+# Vs
+
+Números <- c('uno', 'tres', 'uno', 'cuatro', 'uno', 'cinco', 'uno')
+Resultado <- Números == 'uno'
+Resultado
+
+
+# P10. ----
+
+var_1 <- seq(-5, 5, 1)
+var_2 <- c(-5, 1, 2, -5, 3, 1, 0, -4, -5, 3, 1, -5)
+var_1 <= 0 & var_2 %in% c(-5, 2, -4)
+
+# P11. ----
+
+Serie <- seq(1, 99, 2)
+Multiplo5 <- Serie[Serie%%5 == 0]
+Multiplo5
+
+# P12. ----
+
+Var <- LETTERS[c(1:5)]
+Rep_Var <- Var[rep(c(1:5), each = 2)]
+Rep_Var
+
+# P13. ----
+
+Nueva <- c(11, 20, 15, 10)
+Nombres <- c('xyz', 'c%2', 'mno', 'for')
+names(Nueva) <- Nombres
+Nueva
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"Nombre var" <- 1
+"Nombre var"
+`Nombre var` <- 1
